35239dfa573886b20e9cf9b440eba582c0ed1061
KyleAnthonyHay <haykyle917@gmail.com>
2023-10-21 17:42:49 -0400

    first push

---
+++ TonePod/TonePod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TonePod
+//
+//  Created by Kyle-Anthony Hay on 10/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(red: 253, green: 253, blue: 253, alpha: 1.0) // offwhite #FDFDFD
+    }
+
+
+}
+
